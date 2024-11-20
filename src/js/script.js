fb4cfbf736d962ccc26453bfcf1aa6bd11891868
MaëlieA <maelie@addario.fr>
2024-11-20 09:39:22 +0100

    modification code carousel image taille responsive

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,14 @@
 // *** CAROUSEL ***
 // TODO
 
-const carousel = document.querySelector(".carousel__container");
 const prevButton = document.querySelector(".carousel__button--prev");
 const nextButton = document.querySelector(".carousel__button--next");
+const container = document.querySelector(".carousel__container");
 
-if (carousel) {
- 
-  const scrollAmount = 260;
+prevButton.addEventListener("click", () => {
+  container.scrollBy({ left: -300, behavior: "smooth" }); // Faites défiler vers la gauche
+});
 
-  prevButton.addEventListener("click", function () {
-    carousel.scrollLeft -= scrollAmount; 
-  });
-  nextButton.addEventListener("click", function () {
-    carousel.scrollLeft += scrollAmount; 
-  });
-}
+nextButton.addEventListener("click", () => {
+  container.scrollBy({ left: 300, behavior: "smooth" }); // Faites défiler vers la droite
+});
